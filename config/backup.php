5644e95eee3e544d7cdc20ebc47707fcda6e86fa
Ye Yint Kyaw <yeyintkyaw300@gmail.com>
2023-08-22 22:37:15 +0630

    configure backup and run

--- config/backup.php
+++ config/backup.php
@@ -110,7 +110,7 @@ return [
             /*
              * The filename prefix used for the backup zip file.
              */
-            'filename_prefix' => '',
+            'filename_prefix' => 'DB_Backup_',
 
             /*
              * The disk names on which the backups will be stored.
